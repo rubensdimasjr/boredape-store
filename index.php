e6ba37bd3b52bddc78506a4d5f8d1f6ea29258f4
Rubens Júnior <rubens.jr107@gmail.com>
2022-03-17 00:56:36 -0300

    created grid layout of collection

--- index.php
+++ index.php
@@ -18,17 +18,19 @@
 <body>
 
   <header>
-    <a href="#" class="logo"><i class="fa-brands fa-ethereum"></i></a>
-    <button class="hamburguer" id="hamburguer">
-      <i class="fas fa-bars"></i>
-    </button>
-    <ul class="nav-ul" id="nav-ul">
-      <li><a href="#">Home</a></li>
-      <li><a href="#">Ranking</a></li>
-      <li><a href="#">Sobre</a></li>
-      <li><a href="#">Coleção</a></li>
-      <li><a href="#"><i class="fa-solid fa-circle-user circle-user"></i></a></li>
-    </ul>
+    <nav>
+      <a href="#" class="logo"><i class="fa-brands fa-ethereum"></i></a>
+      <button class="hamburguer" id="hamburguer">
+        <i class="fas fa-bars"></i>
+      </button>
+      <ul class="nav-ul" id="nav-ul">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Ranking</a></li>
+        <li><a href="#">Sobre</a></li>
+        <li><a href="#">Coleção</a></li>
+        <li><a href="#"><i class="fa-solid fa-circle-user circle-user"></i></a></li>
+      </ul>
+    </nav>
   </header>
 
   <main>
@@ -48,6 +50,98 @@
 
   <section id="collection">
     <div class="collection-container">
+
+      <div class="box">
+        <div class="slide-img">
+          <img src="./assets/images/1.png" alt="1"> 
+        </div>
+        <div class="detail-box">
+          <div class="type">
+            <a href="#">Bored Ape 1</a>
+            <span>#8958</span>
+          </div>
+        </div>
+        <div class="detail-box">
+          <a href="#" class="icon-ethereum">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 320 512"><!--! Font Awesome Pro 6.0.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M311.9 260.8L160 353.6 8 260.8 160 0l151.9 260.8zM160 383.4L8 290.6 160 512l152-221.4-152 92.8z"/></svg>
+          </a>
+          <div class="likes">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
+            <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
+          </svg>
+            64
+          </div>
+        </div>
+      </div>
+
+      <div class="box">
+        <div class="slide-img">
+          <img src="./assets/images/1.png" alt="1"> 
+        </div>
+        <div class="detail-box">
+          <div class="type">
+            <a href="#">Bored Ape 1</a>
+            <span>#8958</span>
+          </div>
+        </div>
+        <div class="detail-box">
+          <a href="#" class="icon-ethereum">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 320 512"><!--! Font Awesome Pro 6.0.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M311.9 260.8L160 353.6 8 260.8 160 0l151.9 260.8zM160 383.4L8 290.6 160 512l152-221.4-152 92.8z"/></svg>
+          </a>
+          <div class="likes">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
+            <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
+          </svg>
+            64
+          </div>
+        </div>
+      </div>
+
+      <div class="box">
+        <div class="slide-img">
+          <img src="./assets/images/1.png" alt="1"> 
+        </div>
+        <div class="detail-box">
+          <div class="type">
+            <a href="#">Bored Ape 1</a>
+            <span>#8958</span>
+          </div>
+        </div>
+        <div class="detail-box">
+          <a href="#" class="icon-ethereum">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 320 512"><!--! Font Awesome Pro 6.0.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M311.9 260.8L160 353.6 8 260.8 160 0l151.9 260.8zM160 383.4L8 290.6 160 512l152-221.4-152 92.8z"/></svg>
+          </a>
+          <div class="likes">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
+            <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
+          </svg>
+            64
+          </div>
+        </div>
+      </div>
+
+      <div class="box">
+        <div class="slide-img">
+          <img src="./assets/images/1.png" alt="1"> 
+        </div>
+        <div class="detail-box">
+          <div class="type">
+            <a href="#">Bored Ape 1</a>
+            <span>#8958</span>
+          </div>
+        </div>
+        <div class="detail-box">
+          <a href="#" class="icon-ethereum">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 320 512"><!--! Font Awesome Pro 6.0.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M311.9 260.8L160 353.6 8 260.8 160 0l151.9 260.8zM160 383.4L8 290.6 160 512l152-221.4-152 92.8z"/></svg>
+          </a>
+          <div class="likes">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
+            <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
+          </svg>
+            64
+          </div>
+        </div>
+      </div>
 
       <div class="box">
         <div class="slide-img">
